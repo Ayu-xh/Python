ba6c7afe8c78472ae22553b860b580a17d630774
Ayush Singh <ayushsingh90935@gmail.com>
2026-07-04 23:01:13 +0530

    Added more statistics to each student's profile

--- phase1-core-syntax/project3-gradebook-processor/gradebook.py
+++ phase1-core-syntax/project3-gradebook-processor/gradebook.py
@@ -12,7 +12,11 @@ with open('student.txt', encoding='utf-8') as file:
         split_data = cleaned_line.split(',')
         student_data_dict = {}
         student_data_dict['name'] = split_data[0]
-        student_data_dict['marks'] = split_data[1:]
+        student_data_dict['marks'] = list(map(int, split_data[1:]))
+        student_data_dict['highest_score'] = max(split_data[1:])
+        student_data_dict['lowest_score'] = min(split_data[1:])
+        student_data_dict['average_score'] = round((sum(list(map(int, split_data[1:]))))/len(split_data[1:]), 2)
+
         student_data_list.append(student_data_dict)
 
 
