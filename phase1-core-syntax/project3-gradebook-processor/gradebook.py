53383f30d37d690376fac548aab3318888ff2368
Ayush Singh <ayushsingh90935@gmail.com>
2026-07-04 21:31:59 +0530

    Student data is being stored in the dictionary successfully

--- phase1-core-syntax/project3-gradebook-processor/gradebook.py
+++ phase1-core-syntax/project3-gradebook-processor/gradebook.py
@@ -1,11 +1,22 @@
 # The student.txt file is already created and the format of it is very simple - name, score1, score2, score3.
 # The first step would be to get the file open.
 
+
+# Let us take a new list just for the sake of storing intermediate results.
+student_data_list = []
+
 with open('student.txt', encoding='utf-8') as file:
     file_data = file.readlines()
     for line in file_data:
-        
+        cleaned_line = line.rstrip('\n')
+        split_data = cleaned_line.split(',')
+        student_data_dict = {}
+        student_data_dict['name'] = split_data[0]
+        student_data_dict['marks'] = split_data[1:]
+        student_data_list.append(student_data_dict)
 
 
-# Let us take an empty dictionary for now to store the student data.
-student_data = {}
+print(student_data_list)
+
+
+
