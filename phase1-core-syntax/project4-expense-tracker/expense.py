18d3b976425a8a318940b8b2e9b3b44b46ac0a81
Ayush Singh <ayushsingh90935@gmail.com>
2026-07-07 16:26:00 +0530

    Expense.py file code completed with input validation in prices

--- phase1-core-syntax/project4-expense-tracker/expense.py
+++ phase1-core-syntax/project4-expense-tracker/expense.py
@@ -1,14 +1,18 @@
 # Let us first begin by creating the expenses file.
 # Since, we are going to be working with datetime, let us begin by importing it.
-import datetime
+import datetime as dt
 
 class Expense:
-    def __init__(self, id, name, price, date, type):
+    def __init__(self, id, name, price, date, category):
         self.id = id
         self.name = name
+        if price < 0:
+            raise ValueError(f"Price cannot be negative. Received: {price}")
         self.price = price
-        self.date = datetime.strptime(date, "%Y-%m-%d")
-        self.type = type
+        self.date = dt.datetime.strptime(date, "%Y-%m-%d").date()
+        self.category = category
 
 
-        
+# We will also be using the 'str' dunder method to get a clean, human-readable string as well.
+    def __str__(self):
+        return f"ID: {self.id}\nExpense Name: {self.name}\nPrice: ₹{self.price}\nPurchased on: {self.date}\nExpense Category: {self.category}."
